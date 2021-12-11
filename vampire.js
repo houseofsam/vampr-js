1a52a3febe1654027c8ca0b2a30e327d9ccd2101
houseofsam <samxsaid@gmail.com>
2021-12-11 08:03:24 +0000

    vampr trees intro

--- vampire.js
+++ vampire.js
@@ -10,22 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let count = 0;
+    let currentVampr = this;
 
+    while (currentVampr.creator) {
+      currentVampr = currentVampr.creator;
+      count++;
+    }
+
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
 
+    return false;
   }
 
   /** Stretch **/
@@ -36,9 +49,51 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampr = vampire;
+    let thisCurrentVampr = this;
+    let vampireAncestors = [];
+    let thisVampireAncestors = []
+
+    if (currentVampr.creator === null) {
+      return currentVampr;
+    } else if (thisCurrentVampr.creator === null) {
+      return thisCurrentVampr;
+    }
+  
+
+    while (currentVampr.creator) {
+      vampireAncestors.push(currentVampr.name);
+      currentVampr = currentVampr.creator;
+    }
+    
+    while (thisCurrentVampr.creator) {
+      thisVampireAncestors.push(thisCurrentVampr.name);
+      thisCurrentVampr = thisCurrentVampr.creator;
+    }
+
+    console.log('vamp anc', vampireAncestors);
+    console.log('this vamp anc', thisVampireAncestors);
+
+    // if the ancestors are the same, a vampire is being compared to itself therefore return that vampire.
+    if (JSON.stringify(vampireAncestors) === JSON.stringify(thisVampireAncestors)) {
+      return this;
+    }
+
+    for (let i = 0; i < vampireAncestors.length; i++) {
+      for (let j = 0; j < thisVampireAncestors.length; j++){
+        if (vampireAncestors[i] === thisVampireAncestors[j]) {
+          console.log(vampireAncestors[i]);
+          return vampireAncestors[i];
+        }
+      }
+    }
+
+    return currentVampr;
 
   }
 }
 
 module.exports = Vampire;
+
+
 
